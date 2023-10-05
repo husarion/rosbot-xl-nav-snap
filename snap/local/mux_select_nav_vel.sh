6e528d48273d05d6b6d70921422b46e3feac1146
artivis <jeremie.deray@canonical.com>
2023-10-05 17:49:58 +0200

    use rel path
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- snap/local/mux_select_nav_vel.sh
+++ snap/local/mux_select_nav_vel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-$SNAP/opt/ros/underlay_ws/opt/ros/$ROS_DISTRO/bin/ros2 service call /mux/select topic_tools_interfaces/srv/MuxSelect "{topic: /nav_vel}"
+ros2 service call /mux/select topic_tools_interfaces/srv/MuxSelect "{topic: /nav_vel}"
 exec $@
